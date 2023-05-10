a5c00d333c8850b404c9d8cd0a0d2e4edec29d3f
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-10 13:00:02 -0500

    used tty-table gem to display the products within a table

--- frontend.rb
+++ frontend.rb
@@ -1,7 +1,9 @@
 require "http"
+require "tty-table"
 api_data = HTTP.get("http://localhost:3000/showall.json")
 products_hash = api_data.parse(:json)
 
+table = TTY::Table.new(["ID", "name", "price", "Description"], [])
 products_hash.each do |product|
   puts "ID: #{product["id"]}"
   puts "name: #{product["name"]}"
@@ -9,4 +11,8 @@ products_hash.each do |product|
   puts "Image URL: #{product["image_url"]}"
   puts "Description: #{product["description"]}"
   puts
+
+  table << [product["id"], product["name"], product["price"], product["description"]]
 end
+
+puts table
