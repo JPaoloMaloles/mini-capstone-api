9791f5585b70aa5db02d5397ccc2edf4c0d038fd
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-15 13:01:28 -0500

    used migration to change: description attribute to text data type, changed price to a decimal with precision 3 and scale 2, added inventory attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  #   Create a model method called is_discounted? that returns true if an item is less than or equal to $10 and false otherwise.
+  def is_discounted?
+    return price <= 10
+  end
+
+  # • Create a model method called tax which will return the tax that would be charged for a particular product. (Assume a 9% tax rate.)
+  def tax
+    return price * 0.09
+  end
+
+  # • Create a model method called total which will return the sum of the price + tax
+  def total
+    return price + tax()
+  end
 end
---
+++ db/migrate/20230515175714_change_price_type.rb
@@ -0,0 +1,5 @@
+class ChangePriceType < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, :decimal, precision: 3, scale: 2
+  end
+end
