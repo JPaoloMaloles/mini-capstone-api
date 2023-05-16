096d6ceca888485dbe4346a2610f23b39a0e7cce
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-16 12:24:44 -0500

    added assert response 200 to test create

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,6 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "create" do
     assert_difference "Product.count", 1 do
       post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+      assert_response 200
     end
 
     post "/products.json", params: {}
