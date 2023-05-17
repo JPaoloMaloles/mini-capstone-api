aeefc6c9df5c68ec41b6251de3f92bb0c9a53910
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-17 12:32:06 -0500

    rewrote seeds to handle inventory and supplied id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-product = Product.new(name: "Balloon", price: 9, image_url: "https://cdn.media.amplience.net/i/partycity/C909620_red?$large$&fmt=auto&qlt=default", description: "Helium contained within plastic")
-product.save
-
-product = Product.new(name: "Pencil", price: 1, image_url: "https://weareticonderoga.com/wp-content/uploads/2019/06/13308_GLAMOUR_2000X2000.png", description: "Graphite contained within wood")
-product.save
-
-product = Product.new(name: "Magic Schoolbus", price: 1, image_url: "https://www.tvguide.com/a/img/catalog/provider/1/1/1-3988714139.jpg", description: "Magic contained within bus")
-product.save
-
-supplier = Supplier.new(name: "Best Goods", email: "bestgoods@gmail.com", phone_number: "1-111-111-1111")
+supplier1 = Supplier.new(name: "Best Goods", email: "bestgoods@gmail.com", phone_number: "1-111-111-1111")
 supplier.save
 
-supplier = Supplier.new(name: "Worst Goods", email: "worstgoods@gmail.com", phone_number: "2-222-222-2222")
+supplier2 = Supplier.new(name: "Worst Goods", email: "worstgoods@gmail.com", phone_number: "2-222-222-2222")
 supplier.save
+#-----------------------------------
+product = Product.new(name: "Balloon", price: 9, image_url: "https://cdn.media.amplience.net/i/partycity/C909620_red?$large$&fmt=auto&qlt=default", description: "Helium contained within plastic", inventory: 4, supplier_id: supplier1.id)
+product.save
+
+product = Product.new(name: "Pencil", price: 1, image_url: "https://weareticonderoga.com/wp-content/uploads/2019/06/13308_GLAMOUR_2000X2000.png", description: "Graphite contained within wood", inventory: 4, supplier_id: supplier1.id)
+product.save
+
+product = Product.new(name: "Magic Schoolbus", price: 1, image_url: "https://www.tvguide.com/a/img/catalog/provider/1/1/1-3988714139.jpg", description: "Magic contained within bus", inventory: 4, supplier_id: supplier2.id)
+product.save
+
+# product.create!({
+
+# })
