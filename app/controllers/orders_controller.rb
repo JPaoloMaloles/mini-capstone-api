1b6868cb5ee4127116b3a193b617c34d3fd53bf6
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-22 13:00:31 -0500

    added implemented quantity to the calculations of the orders_controller create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,9 +24,9 @@ class OrdersController < ApplicationController
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: @product.price,
-      tax: @product.tax,
-      total: @product.total,
+      subtotal: @product.price * params["quantity"].to_i,
+      tax: @product.tax * params["quantity"].to_i,
+      total: @product.total * params["quantity"].to_i,
     )
     render :show
   end
