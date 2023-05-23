1189eaa7f5ca6f3fff95851348e92c31c0a82d3b
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-23 11:23:55 -0500

    added code from orders_controller test to products_controller test that signs in user, set that code's admin to true, then implemented headers into the tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,6 +4,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+    @order = Order.create(user_id: @user.id, product_id: Product.first.id, quantity: 10)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -22,29 +30,29 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }
+      post "/products.json", params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }, headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
 
-    post "/products.json", params: {}
+    post "/products.json", params: {}, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
   end
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    patch "/products/#{product.id}.json", params: { name: "" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
   end
 
   test "delete" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
   end
